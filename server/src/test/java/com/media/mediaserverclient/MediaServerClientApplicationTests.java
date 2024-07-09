8d585f965ef33e3e7dbffd81fe756618d725a03a
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-09 13:15:50 +0200

    Added missing files

---
+++ server/src/test/java/com/media/mediaserverclient/MediaServerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.mediaserverclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
