c2687231ee4e1a5573deb47af4a7186e13f2ccb0
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-12 12:23:31 +0200

    Added REST requests to main.py

--- fast-server/main.py
+++ fast-server/main.py
@@ -1,14 +1,38 @@
 from typing import Any, Union
 from fastapi import FastAPI
 
+from video_utility.video_model import Video
+
 app = FastAPI()
+
+video_db: list[Video] = []
+
+# TODO add all exising videos to video_db
 
 
 @app.get("/")
 async def read_root() -> dict[str, str]:
+    # TODO return proper starting page
     return {"Hello": "World"}
 
+    
+# Why `put` instead of `post`? 
+# See: https://restfulapi.net/rest-put-vs-post/
+@app.put('/videos/download_video')
+async def download_video(video: Video) -> Video:
+    """Downloads the video onto the server for later retrieval
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, Any]:
-    return {"item_id": item_id, "q": q}
+    Parameters
+    ----------
+    url : str
+        the source URL of the video
+
+    Returns
+    -------
+    JSON
+        Returns a description of the success and infos of the downloaded video
+    """
+    # TODO validate URL
+    # TODO download video using yt-dlp
+    # TODO return meaningful response, handle errors too!
+    return video
