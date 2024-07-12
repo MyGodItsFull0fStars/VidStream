54568eb6adb31263bc10b09c29a4523c9dc00a87
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-12 12:21:53 +0200

    Added video_utility file and utility functions

---
+++ fast-server/video_utility/video_utility.py
@@ -0,0 +1,36 @@
+
+
+def download_video_from_url(url: str) -> bool:
+    """Download video from given URL
+
+    Parameters
+    ----------
+    url : str
+        the videos URL
+
+    Returns
+    -------
+    bool
+        return `True` if the download was successful, else return `False`
+    """
+    
+    # only return True if the download was successful
+    return True
+
+def encode_video(video_file_path: str) -> bool:
+    """Loads the video from the file path and encodes it into multiple representations
+
+    Parameters
+    ----------
+    video_file_path : str
+        the location of the video file on the server
+
+    Returns
+    -------
+    bool
+        return `True` if the video could be encoded, else return `False`
+    """
+    # TODO encode the video into multiple representations
+    
+    # only return True if the encoding was successful
+    return True
