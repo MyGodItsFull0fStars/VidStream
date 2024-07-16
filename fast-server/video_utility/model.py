9975714c6fc93205ac82d51bcf0db6e0781ef252
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-16 15:43:03 +0200

    Added TODO for REST server models

--- fast-server/video_utility/model.py
+++ fast-server/video_utility/model.py
@@ -2,10 +2,24 @@ from pydantic import BaseModel
 
 
 class URL(BaseModel):
+    """TODO Add description
+
+    Parameters
+    ----------
+    BaseModel : URL
+        TODO
+    """
     url: str
 
 
 class Video(BaseModel):
+    """TODO Add description
+
+    Parameters
+    ----------
+    BaseModel : Video
+        TODO
+    """
 
     name: str
     path: str
