f193ec3ceccb978aeef5e07cf4dff12d1ec0e223
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-17 11:48:45 +0200

    Added url_validation to URL class

--- fast-server/video_utility/model.py
+++ fast-server/video_utility/model.py
@@ -1,4 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
+import re
+
+# Validate URL
+url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
 
 
 class URL(BaseModel):
@@ -10,6 +15,17 @@ class URL(BaseModel):
         TODO
     """
     url: str
+
+    @field_validator('url')
+    @classmethod
+    def url_validationn(cls, v: str) -> str:
+        if v is None or len(v) == 0:
+            raise ValueError('empty url')
+
+        if re.match(url_pattern, v) is None:
+            raise ValueError('invalid url pattern')
+
+        return v
 
 
 class Video(BaseModel):
@@ -23,3 +39,8 @@ class Video(BaseModel):
 
     name: str
     path: str
+
+
+if __name__ == '__main__':
+    # if it crashes, there is something with the URL validation
+    URL(url='https://www.youtube.com/watch?v=RAjgkqJ-8xQ')
