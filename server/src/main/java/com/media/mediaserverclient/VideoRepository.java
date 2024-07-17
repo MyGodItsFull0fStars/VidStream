d57fe9738f8059480c3575615f1bf516fe645abe
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-17 10:44:59 +0200

    Removed Java

--- server/src/main/java/com/media/mediaserverclient/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.media.mediaserverclient;
-
-import com.media.mediaserverclient.model.Video;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VideoRepository extends CrudRepository<Video, Integer> {
-
-    Video findVideoById(Integer id);
-}
