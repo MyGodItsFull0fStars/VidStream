f32276e86ecfbe30fc39217b6b9aef4cfcb5e31a
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-24 09:47:11 +0200

    Added todos

--- fast-server/video_utility/video_utility.py
+++ fast-server/video_utility/video_utility.py
@@ -1,6 +1,6 @@
 
 
-def download_video_from_url(video_name: str, url: str) -> bool:
+def download_video_from_url(url: str) -> bool:
     """Download video from given URL
 
     Parameters
@@ -13,11 +13,16 @@ def download_video_from_url(video_name: str, url: str) -> bool:
     bool
         return `True` if the download was successful, else return `False`
     """
-    
+    # TODO check if video was already downloaded before (check with URL)
+    # TODO download video
+    # TODO once downloaded, add to video_db
+    # TODO convert to .mp4 if necessary
+
     # only return True if the download was successful
     # os.system(url)
-    
+
     return True
+
 
 def encode_video(video_file_path: str) -> bool:
     """Loads the video from the file path and encodes it into multiple representations
@@ -33,6 +38,6 @@ def encode_video(video_file_path: str) -> bool:
         return `True` if the video could be encoded, else return `False`
     """
     # TODO encode the video into multiple representations
-    
+
     # only return True if the encoding was successful
     return True
